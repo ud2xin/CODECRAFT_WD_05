9c396d85b161b2d8218edb5e73dc81c6f97f5612
Muhammad Haikal Syamsuddin <udingaming0903@gmail.com>
2025-08-31 15:49:41 +0700

    api key

--- public/js/config.js
+++ public/js/config.js
@@ -1,1 +1,3 @@
 OPENWEATHER_API_KEY = "d30c1eca3ad823c5398519077c8a6548";
+// because this is frontend code, in real apps, never expose your API keys like this.
+// Use a backend server to keep them secret.
